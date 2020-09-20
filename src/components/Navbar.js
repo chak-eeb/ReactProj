71090d21c2b4f2098654140f946fdff67de389cb
Chakib Atmane <chakib@Chakibs-MacBook-Pro.local>
2020-09-20 06:53:47 -0400

    add hamburger menu to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <></>;
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            LOGO <i className="fab fa-typo3"></i>
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
